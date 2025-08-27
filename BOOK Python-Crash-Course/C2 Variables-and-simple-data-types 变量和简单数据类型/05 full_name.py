c01cef56f160503249bd16c15927de9a09041ffb
xJasonShane <jenson_shan@yeah.net>
2025-08-27 16:14:06 +0800

    feat: 添加Python基础示例代码
    添加Python入门示例代码，包括变量定义、字符串操作等基础语法演示

---
+++ BOOK Python-Crash-Course/C2 Variables-and-simple-data-types 变量和简单数据类型/05 full_name.py
@@ -0,0 +1,15 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name= f"{first_name} {last_name}"
+print(full_name)
+
+first_name = "ada"
+last_name = "lovelace"
+full_name= f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
