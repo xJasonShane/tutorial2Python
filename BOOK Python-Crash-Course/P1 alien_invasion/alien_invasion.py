df44bdeb719dabc892bda1459dd922e4bdacca5c
xJasonShane <jenson_shan@yeah.net>
2025-09-23 18:54:08 +0800

    feat: 添加飞船类并实现左右移动功能
    添加Ship类用于管理飞船图像和位置，并在主游戏中实现键盘控制飞船左右移动的功能

--- BOOK Python-Crash-Course/P1 alien_invasion/alien_invasion.py
+++ BOOK Python-Crash-Course/P1 alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -15,19 +16,39 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screem_height)
         )
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
     
     def run_game(self):
         """开始游戏的主循环"""
-        while True:
-            # 监视键盘和鼠标事件
-            for event in pygame.event.get():
+        while True: 
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            # 每次循环时都重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-            
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+    
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
